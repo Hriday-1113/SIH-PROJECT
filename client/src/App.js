98d08a55223ea3ec32e2f5ec51dc5d1b92e4302f
Farzan Irani <farzanirani2004@gmail.com>
2024-08-31 13:40:52 +0530

    Setting up the layout and file structure

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
